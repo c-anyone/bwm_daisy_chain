babaabdad144b76b97bcfd5d78597b4941c0a69e
Fabio Pungg <f.pungg@mci4me.at>
2017-03-24 15:03:58 +0100

    developed a basic state machine design

--- StateMachine/machine_state.c
+++ StateMachine/machine_state.c
@@ -2,20 +2,64 @@
  * machine_state.c
  *
  *  Created on: Mar 24, 2017
- *      Author: faebsn
+ *      Author: fapungg
  */
 
 #include "machine_state.h"
 
-static void defaultStop(MachineStatePtr state) {
-
+machine_state_t state_machine(machine_state current_state) {
+	states[current_state]->stateFun();
+	return states[current_state]->transFun();
 }
 
-static void defaultStart(MachineStatePtr state) {
-
+void init() {
+	// send out init signal to everyone
 }
 
-void defaultImplementation(MachineStatePtr state) {
-	state->start = defaultStart;
-	state->stop = defaultStop;
+machine_state_t init_to_waiting() {
+	// check if all init conditions are set
+}
+
+void waiting() {
+	// move sled to intake position
+}
+
+machine_state_t waiting_to_intake_pos() {
+	// check if sled is in position
+}
+
+void intake_pos() {
+	// raise intake
+}
+
+machine_state_t intake_pos_to_ready() {
+	// check if intake done raising
+}
+
+void intake_ready() {
+	// begin moving sled to shot ready position
+}
+
+machine_state_t intake_ready_to_shot_ready(){
+	// check if sled is done positioning
+}
+
+void  shot_ready() {
+	// wait for external shoot signal
+}
+
+machine_state_t shot_ready_to_fired() {
+	// check if sled is in fired position
+}
+
+void shot_fired() {
+	// move sled back to waiting position
+	// rotate magazine
+	// lower intake
+}
+
+machine_state_t shot_fired_to_waiting() {
+	// sled in position
+	// magazine in position
+	// intake lowered
 }
--- StateMachine/machine_state.h
+++ StateMachine/machine_state.h
@@ -2,27 +2,68 @@
  * machine_state.h
  *
  *  Created on: Mar 24, 2017
- *      Author: faebsn
+ *      Author: fapungg
  */
 
 #ifndef MACHINE_STATE_H_
 #define MACHINE_STATE_H_
 
-typedef struct MachineState* MachineStatePtr;
+typedef enum {
+    INIT,
+    WAITING,
+    INTAKE_POS,
+    INTAKE_READY,
+    SHOT_READY,
+    SHOT_FIRED,
+    NUM_STATES
+} machine_state_t;
 
-typedef void (*EventStartFunc)(MachineStatePtr);
-typedef void (*EventStopFunc)(MachineStatePtr);
+typedef void(*StateEntryFun)();
+typedef void(*StateFun)();
+typedef machine_state_t(*TransitionFun)();
 
-void defaultImplementation(MachineStatePtr);
 
-struct MachineState {
-	EventStartFunc start;
-	EventStopFunc stop;
+typedef struct {
+	machine_state_t current_state;
+	machine_state_t next_state;
+	StateFun stateFun;
+	TransitionFun transFun;
+}state_t;
+
+
+
+void state_machine(machine_state_t*);
+
+/**
+ * State Functions
+ */
+void init();
+void waiting();
+void intake_pos();
+void intake_ready();
+void shot_ready();
+void shot_fired();
+
+/**
+ * Transition Functions
+ */
+void init_to_waiting();
+void waiting_to_intake_pos();
+void intake_pos_to_ready();
+void intake_ready_to_shot_ready();
+void shot_ready_to_fired();
+void shot_fired_to_waiting();
+
+state_t states[NUM_STATES] = {
+		{INIT, WAITING, &init, &init_to_waiting},
+		{WAITING, INTAKE_POS, &waiting, &waiting_to_intake_pos},
+		{INTAKE_POS, INTAKE_READY, &intake_pos, &intake_pos_to_ready},
+		{INTAKE_READY, SHOT_READY, &intake_ready, &intake_ready_to_shot_ready},
+		{SHOT_READY, SHOT_FIRED, &shot_ready, &shot_ready_to_fired},
+		{SHOT_FIRED, SHOT_READY, &shot_fired, &shot_fired_to_waiting}
+
 };
 
-enum StateMachineEnum {
-	STATE_1,
-	STATE_2
-};
+machine_state_t current_state = INIT;
 
 #endif /* MACHINE_STATE_H_ */
